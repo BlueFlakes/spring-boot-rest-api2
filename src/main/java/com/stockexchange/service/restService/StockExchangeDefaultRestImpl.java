9dcfdc54588418774f3cb53eeff5d90b72e440c0
Kamil <blueflamesxcode@gmail.com>
2018-01-16 21:52:57 +0100

    add default stock exchange rest service impl

---
+++ src/main/java/com/stockexchange/service/restService/StockExchangeDefaultRestImpl.java
@@ -0,0 +1,14 @@
+package com.stockexchange.service.restService;
+
+import com.stockexchange.dao.StockExchangeDao;
+import com.stockexchange.model.StockExchange;
+import com.stockexchange.service.ObjectFieldValueSwapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockExchangeDefaultRestImpl extends DefaultRestServiceImpl<StockExchange, StockExchangeDao> {
+    public StockExchangeDefaultRestImpl(StockExchangeDao stockExchangeDao,
+                                        ObjectFieldValueSwapper<StockExchange> fieldValueSwapper) {
+        super(stockExchangeDao, fieldValueSwapper);
+    }
+}
