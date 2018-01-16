2aad7a9a82518799eb1dbd1701b87d56a3417dec
Kamil <blueflamesxcode@gmail.com>
2018-01-16 21:52:34 +0100

    Add share default rest service impl

---
+++ src/main/java/com/stockexchange/service/restService/ShareDefaultRestImpl.java
@@ -0,0 +1,13 @@
+package com.stockexchange.service.restService;
+
+import com.stockexchange.dao.ShareDao;
+import com.stockexchange.model.Share;
+import com.stockexchange.service.ObjectFieldValueSwapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShareDefaultRestImpl extends DefaultRestServiceImpl<Share, ShareDao> {
+    public ShareDefaultRestImpl(ShareDao shareDao, ObjectFieldValueSwapper<Share> fieldValueSwapper) {
+        super(shareDao, fieldValueSwapper);
+    }
+}
