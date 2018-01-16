83eb3c2e7f1814a1fe42abbe377abc92f586eeab
Kamil <blueflamesxcode@gmail.com>
2018-01-16 21:52:16 +0100

    Add customer default rest service impl

---
+++ src/main/java/com/stockexchange/service/restService/CustomerDefaultRestImpl.java
@@ -0,0 +1,13 @@
+package com.stockexchange.service.restService;
+
+import com.stockexchange.dao.CustomerDao;
+import com.stockexchange.model.Customer;
+import com.stockexchange.service.ObjectFieldValueSwapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerDefaultRestImpl extends DefaultRestServiceImpl<Customer, CustomerDao> {
+    public CustomerDefaultRestImpl(CustomerDao customerDao, ObjectFieldValueSwapper<Customer> valueSwapper) {
+        super(customerDao, valueSwapper);
+    }
+}
