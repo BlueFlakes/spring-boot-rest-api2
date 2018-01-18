cab1c7999f0bbe03349c26ee921c367fe66f7d5c
Kamil <blueflamesxcode@gmail.com>
2018-01-18 17:04:06 +0100

    add InvalidDataFormatException

---
+++ src/main/java/com/stockexchange/exception/InvalidDataFormatException.java
@@ -0,0 +1,8 @@
+package com.stockexchange.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invalid data format.")
+public class InvalidDataFormatException extends AppCustomException {
+}
