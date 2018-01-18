29e9f2e2d6d3d043d9b4a5c5d71ac5cb94690a7c
Kamil <blueflamesxcode@gmail.com>
2018-01-18 18:33:14 +0100

    clear imports

--- src/main/java/com/stockexchange/exception/AppCustomException.java
+++ src/main/java/com/stockexchange/exception/AppCustomException.java
@@ -1,10 +1,6 @@
 package com.stockexchange.exception;
 
-import com.stockexchange.model.ErrorCode;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.Optional;
 
 public class AppCustomException extends Exception {
     public ErrorCode getErrorCode() {
